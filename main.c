1de6aa47aafd8ab55a2d94b70f7a6b300a7667c2
226205 <aborecki@op.pl>
2018-12-18 00:39:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -19,6 +19,8 @@ void menu();
 //class Annealing();
 void AnnMenu();
 int SimAnn(double, double, float);
+void TabuMenu();
+int TabuSearch(int);
 
 
 
@@ -63,6 +65,7 @@ void chosingfile()
     Writetab(distances);
 }
 
+
 bool fileread(std::string filename)         // funkcja do odczytywania danych z pliku
 {
     std::fstream plik;
@@ -81,6 +84,7 @@ bool fileread(std::string filename)         // funkcja do odczytywania danych z 
     return 0;
 }
 
+
 void Writetab(int** cities)
 {
     std::cout << "\n\n";
@@ -92,6 +96,7 @@ void Writetab(int** cities)
         std::cout<<"\n";
     }
 }
+
 
 void menu()
 {
@@ -119,8 +124,10 @@ void menu()
             break;
         }
         case '7':
-
+        {
+            TabuMenu();
             break;
+        }
         case '0':
             break;
         default:
@@ -131,6 +138,7 @@ void menu()
         }
     }
 }
+
 
 void AnnMenu()
 {
@@ -147,7 +155,7 @@ void AnnMenu()
     float annealing = 0;
     int choice = -1;
     while (choice != '0'){
-        std::cout << "\n Symulowane wyrzazanie, ustaw porzadane parametry:\n 1. temperatureMax = " << temperatureMax << ", temperatureMin = " << temperatureMin << "\n 2. Stopien wyrzazania = " << annealing << "\n 6. Uruchom algorytm\n 0. Powrot do menu glownego\n Twoj wybor: ";
+        std::cout << "\n Symulowane wyrzazanie, ustaw porzadane parametry:\n 1. temperatureMax = " << temperatureMax << ", temperatureMin = " << temperatureMin << "\n 2. Stopien wyrzazania = " << annealing << "\n 6. Uruchom algorytm\n 8. Wyswietl wyniki\n 9. Wyczysc wyniki\n 0. Powrot do menu glownego\n Twoj wybor: ";
         choice = getche();
         switch(choice){
             case '1':{
@@ -169,12 +177,11 @@ void AnnMenu()
                         std::cin >> annealing;
                     }
                     else if (choice2 == '2') annealing = 0.999;
-                    else    std::cout << "\nCos poszlo zle, sprobuj ponownie.";
-                } while (annealing < 0 && annealing > 1);
+                } while (annealing <= 0 || annealing >= 1);
                 break;}
 
             case '6':{
-                if(annealing < 0 && annealing > 1)
+                if(annealing <= 0 || annealing >= 1)
                     std::cout << "\n Stopien wyrzazania nie zostal ustawiony, sprobuj ponownie.\n";
                 else{
                     std::cout << "\n Wpisz ilosc powtorzen: ";
@@ -186,11 +193,21 @@ void AnnMenu()
                         results[newResult]++;
                     }
                     std::clock_t end = clock();
-                    std::cout<<"\n\n\nCzas wykonania wszystkich prob: " << double(end - begin) / CLOCKS_PER_SEC << "\n\nproba srednio trwala: "<< (double(end - begin) / CLOCKS_PER_SEC) / numberOfRepeats << "\n Ilosc wystapien poszczegolnych wynikow:\n";
-                    for(int i = 0; i < resultsSpread; i++)
-                        if(results[i] > 0)
-                            std::cout << i << ": " << results[i] << "\n";
+                    std::cout<<"\n\n\nCzas wykonania wszystkich prob: " << double(end - begin) / CLOCKS_PER_SEC << "\n\nproba srednio trwala: "<< (double(end - begin) / CLOCKS_PER_SEC) / numberOfRepeats << "\n";
                 }
+                break;
+            }
+            case '8':{
+                std::cout << "\n Ilosc wystapien poszczegolnych wynikow:\n";
+                for(int i = 0; i < resultsSpread; i++)
+                    if(results[i] > 0)
+                        std::cout << i << ": " << results[i] << "\n";
+                break;
+            }
+            case '9':{
+            for(int i = 0; i < resultsSpread; i++)
+                results[i] = 0;
+            break;
             }
             default:
                 break;
@@ -198,6 +215,7 @@ void AnnMenu()
     }
     delete [] results;
 }
+
 
 int SimAnn(double temperatureMax, double temperatureMin, float annealing)
 {
@@ -216,44 +234,43 @@ int SimAnn(double temperatureMax, double temperatureMin, float annealing)
 	for (int i = 0; i < cityamount + 1; i++)
 		bestPath[i] = tempBestPath[i];
 
-	int* tempP = new int [cityamount + 1];
-    int cityA, cityB, temp, sumA, diff;
+	int* tempPath = new int [cityamount + 1];
+
+    int cityA, cityB, temp, tempCost, diff;
     int iteracja = 0;
     while (temperatureMax > temperatureMin) {
         iteracja++;
         for (int i = 0; i <= cityamount; i++)
-            tempP[i] = tempBestPath[i];
+            tempPath[i] = tempBestPath[i];
         do {
             cityA = (rand() % (cityamount - 1)) + 1;
             cityB = (rand() % (cityamount - 1)) + 1;
         } while (cityA == cityB);
 
-        temp = tempP[cityA];
-        tempP[cityA] = tempP[cityB];
-        tempP[cityB] = temp;
+        temp = tempPath[cityA];
+        tempPath[cityA] = tempPath[cityB];
+        tempPath[cityB] = temp;
 
-
-
-        sumA =  diff = 0;
+        tempCost =  diff = 0;
         for(int i = 0; i < cityamount; i++){
-            sumA += distances[tempP[i]][tempP[i + 1]];
+            tempCost += distances[tempPath[i]][tempPath[i + 1]];
         }
-        diff = sumA - tempBestCost;
+        diff = tempCost - tempBestCost;
 
         if(((double)rand() / (RAND_MAX)) < exp((-1)*diff / temperatureMax) || diff < 0){  // zabezpieczenie przed przekreceniem inta
 //            std::cout << "\n\n123456\n\n";
             tempBestCost = 0;
             for(int i = 0; i < cityamount; i++){
-                tempBestPath[i] = tempP[i];
-                tempBestCost += distances[tempP[i]][tempP[i + 1]];
+                tempBestPath[i] = tempPath[i];
+                tempBestCost += distances[tempPath[i]][tempPath[i + 1]];
             }
-            if(sumA < bestCost){    //sumA bo byla liczona dla temp ktory stal sie nowym tempBestPath
-                bestCost = sumA;
+            if(tempCost < bestCost){    //tempCost bo byla liczona dla temp ktory stal sie nowym tempBestPath
+                bestCost = tempCost;
                 for (int i = 0; i < cityamount + 1; i++)
                     bestPath[i] = tempBestPath[i];
             }
         }
-        temperatureMax = temperatureMax * annealing;
+        temperatureMax = temperatureMax * annealing;    // redukcja geometryczna
 	}
 
 //    std::cout<<"\n\nNajkrotsza odnaleziona droga przez wszystkie miasta to:\n";
@@ -265,6 +282,145 @@ int SimAnn(double temperatureMax, double temperatureMin, float annealing)
 
 	delete[] tempBestPath;
 	delete[] bestPath;
+	delete[] tempPath;
 
 	return bestCost;
+}
+
+
+void TabuMenu()
+{
+    int resultsSpread = 1000;
+    int newResult = 0;
+
+    int numberOfRepeats = 0;
+    int* results = new int[resultsSpread];
+    for(int i = 0; i < resultsSpread; i++)
+        results[i] = 0;
+
+
+    int tabuSize = 0;
+
+
+    int choice = -1;
+    while (choice != '0'){
+        std::cout << "\n Tabu Search, ustaw porzadane parametry:\n 2. Wielkosc tablicy Tabu: " << tabuSize << "\n 7. Uruchom algorytm\n 8. Wyswietl wyniki\n 9. Wyczysc wyniki\n 0. Powrot do menu glownego\n Twoj wybor: ";
+        choice = getche();
+        switch(choice){
+            case '1':{
+//                do {
+//                    std::cout << "\n\nPodaj minimalna temperature: ";
+//                    std::cin >> temperatureMin;
+//                    std::cout << "Podaj maksymalna temperature: ";
+//                    std::cin >> temperatureMax;
+//                    if(temperatureMax <= temperatureMin) std::cout << "Temperatura minimalna musi byc mniejsza od maksymalnej";
+//                } while (temperatureMax <= temperatureMin);
+                break;}
+
+                //(annealing <= 0 || annealing >= 1);
+
+            case '2':{
+                do{
+                    std::cout << "\n\nPodaj wielkosc tablicy Tabu\n 1. Wpisz wlasna\n 2. Wybierz domyslna '" << cityamount-1 << "'\nTwoj wybor:  ";
+                    int choice2 = getche();
+                    if(choice2 == '1'){
+                        std::cout << "\nPodaj wielkosc tablicy Tabu ( zakres: 1 -> " << (cityamount-1)*(cityamount-1)-1 << " : ";
+                        std::cin >> tabuSize;
+                    }
+                    else if (choice2 == '2') tabuSize = cityamount-1;
+                    else    std::cout << "\nCos poszlo zle, sprobuj ponownie.";
+                } while (tabuSize < 1 || tabuSize > (cityamount-1)*(cityamount-1)-1 );
+                break;}
+
+            case '7':{
+                if(tabuSize < 1 || tabuSize > (cityamount-1)*(cityamount-1)-1)
+                    std::cout << "\n Wielkosc tablicy Tabu nie zostala ustawiona, sprobuj ponownie.\n";
+                else{
+                    std::cout << "\n Wpisz ilosc powtorzen: ";
+                    std::cin >> numberOfRepeats;
+                    clock_t begin = clock();
+
+                    for(int i = 0; i < numberOfRepeats; i++){
+                        newResult = TabuSearch(tabuSize);
+                        results[newResult]++;
+                    }
+                    std::clock_t end = clock();
+                    std::cout<<"\n\n\nCzas wykonania wszystkich prob: " << double(end - begin) / CLOCKS_PER_SEC << "\n\nproba srednio trwala: "<< (double(end - begin) / CLOCKS_PER_SEC) / numberOfRepeats << "\n";
+                }
+                break;
+            }
+            case '8':{
+                std::cout << "\n Ilosc wystapien poszczegolnych wynikow:\n";
+                for(int i = 0; i < resultsSpread; i++)
+                    if(results[i] > 0)
+                        std::cout << i << ": " << results[i] << "\n";
+                break;
+            }
+            case '9':{
+            for(int i = 0; i < resultsSpread; i++)
+                results[i] = 0;
+            break;
+            }
+            default:
+                break;
+        }
+    }
+    delete [] results;
+}
+
+
+int TabuSearch(int tabuSize)
+{
+	int* bestPath = new int[cityamount + 1];      //sciezka i waga optymalnego rozwiazania
+	int bestCost = INT_MAX;
+
+	int* tempBestPath = new int[cityamount + 1];  //sciezka i waga optymalnego rozwiazania tymczasowego
+	int tempBestCost = INT_MAX;
+
+    int* tempPath = new int [cityamount + 1];
+    for(int i = 0; i <= cityamount; i++)
+        tempPath[i] = 0;
+
+    int tempCost = INT_MAX;
+
+    //setTempBestPath on random
+	tempBestPath[0] = tempBestPath[cityamount] = 0;
+
+    bool* random = new bool[cityamount + 1];
+    random[0] = random[cityamount] = true;
+    for(int i = 1; i < cityamount; i++)
+        random[i] = false;
+    int tempR, fA, tA;
+
+    for (int i = 1; i < cityamount; i++){
+        fA = tA = 0;
+        tempR = (rand() % (cityamount - i)) + 1;
+        for(int j = 1; fA < tempR; j++){
+            if(random[j] == false)
+                fA++;
+            else
+                tA++;
+        }
+        std::cout << "\n tempr: " << tempR << "  tA" << tA;
+        random[tempR + tA] = true;
+        tempBestPath[i] = tempR + tA;
+    }
+    delete[] random;
+
+	//setBestPath
+	for (int i = 0; i < cityamount + 1; i++)
+		bestPath[i] = tempBestPath[i];
+
+
+    std::cout<<"\n\nNajkrotsza odnaleziona droga przez wszystkie miasta to:\n";
+    for(int i = 0; i < cityamount; i++)
+        std::cout << bestPath[i] << " -> ";
+    std::cout << bestPath[cityamount];
+    std::cout<<"\nJej calkowity dystans wynosi: " << bestCost;
+
+    delete[] tempBestPath;
+	delete[] bestPath;
+	delete[] tempPath;
+
+return 0;
 }
